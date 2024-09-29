51c5e142203836fc28c7de8acc1e7de74dacd5f2
hanhaoxin <hanhaoxin2008@outlook.com>
2024-09-29 18:18:41 +0800

    增加矩阵行列式

--- src/main/java/org/xyai/matrix/MatrixCalculator.java
+++ src/main/java/org/xyai/matrix/MatrixCalculator.java
@@ -1,6 +1,15 @@
 package org.xyai.matrix;
 import org.xyai.matrix.Matrix;
 public class MatrixCalculator {
+
+
+    /**
+     * 矩阵加法
+     * @param a
+     * @param b
+     * @return a+b
+     * @throws Exception
+     */
     public static Matrix add(Matrix a, Matrix b) throws Exception {
 
         //检查矩阵是否同型
@@ -22,7 +31,14 @@ public class MatrixCalculator {
             return result;
         }
 
-        public static Matrix sub(Matrix a, Matrix b) throws Exception {
+    /**
+     * 矩阵减法
+     * @param a
+     * @param b
+     * @return a-b
+     * @throws Exception
+     */
+    public static Matrix sub(Matrix a, Matrix b) throws Exception {
             //检查矩阵是否同型
             if (a.getRows() != b.getRows() || a.getCols() != b.getCols()) {
                 throw new Exception("matrices are not the same size");
@@ -35,8 +51,46 @@ public class MatrixCalculator {
                     result.set(i, j, a.get(i, j) - b.get(i, j));
             }
             return result;
+
+
         }
 
+    public static Matrix subMatrix(Matrix m, int row, int col) throws Exception {
+
+        Matrix result = new Matrix(m.getRows() - 1, m.getCols() - 1);
+        for (int i = 1; i <= m.getRows(); i++) {
+            for (int j = 1; j <= m.getCols(); j++) {
+                if (i != row && j != col) {
+                    if (i > 1 && j > 1) {
+                        result.set(i - 1, j - 1, m.get(i, j));
+                    } else if (i >1) {
+                        result.set(i - 1, j, m.get(i, j));
+                    } else if (j>1) {
+                        result.set(i, j - 1, m.get(i, j));
+                    }else {
+                        result.set(i , j , m.get(i, j));
+                    }
+
+                }
+            }
+        }
+        return result;
+    }
+
+    //矩阵行列式
+    public static double det(Matrix a) throws Exception {
+        if (a.getRows() != a.getCols()) {
+            throw new Exception("matrix is not square");
+        }
+        if (a.getRows() == 1) {
+            return a.get(1, 1);
+        }
+        double result = 0;
+        for (int i = 1; i <= a.getCols(); i++) {
+            result += Math.pow(-1, i + 1) * a.get(1, i) * det(subMatrix(a, 1, i));
+        }
+        return result;
+    }
 
 
 
--- src/test/java/xyaiTest/matrixTest/MatrixCalculatorTest.java
+++ src/test/java/xyaiTest/matrixTest/MatrixCalculatorTest.java
@@ -29,4 +29,29 @@ public class MatrixCalculatorTest {
         }
     }
 
+
+    @Test
+    public void testsubMatrix() {
+        String data="1 1 3\n"+"1 1 3\n"+"3 3 3";
+        try {
+            Matrix a = new Matrix(3, 3,data );
+            Matrix c = MatrixCalculator.subMatrix(a, 1, 2);
+            System.out.println(c);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testDet() {
+        String data="1 2 \n"+"1 1 ";
+        try {
+            Matrix a = new Matrix(2, 2,data );
+            double c = MatrixCalculator.det(a);
+            System.out.println(c);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
